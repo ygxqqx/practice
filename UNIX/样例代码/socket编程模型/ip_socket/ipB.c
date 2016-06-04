6aab596790607b8929eb9a89cdecd173ac1fdff5
ygxqqx <ygxqqx@163.com>
2016-06-04 19:27:39 +0800

    midify

--- UNIX/样例代码/socket编程模型/ip_socket/ipB.c
+++ UNIX/样例代码/socket编程模型/ip_socket/ipB.c
@@ -6,27 +6,24 @@
 //1
 #include <netinet/in.h>
 #include <arpa/inet.h>
-main()
-{
+void main() {
 	int fd;
 	int r;
 	//2
-	struct sockaddr_in addr={0};
+	struct sockaddr_in addr = {0};
 	//1.建立socket
 	//3
-	fd=socket(AF_INET,SOCK_DGRAM,0);
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
 	//2.连接到指定的地址
 	//4
-	addr.sin_family=AF_INET;
-	addr.sin_port=htons(9999);
-	addr.sin_addr.s_addr
-		=inet_addr("192.168.180.92");
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(9999);
+	addr.sin_addr.s_addr = inet_addr("192.168.180.92");
 	
-	r=connect(fd,(struct sockaddr*)&addr,
-			sizeof(addr));
+	r = connect(fd, (struct sockaddr*)&addr, sizeof(addr));
 	//3.发送数据
-	write(fd,"Hello!Maomaochong!",
-		strlen("Hello!Maomaochong!"));
+	write(fd, "Hello!Maomaochong!", strlen("Hello!Maomaochong!"));
 	//4.关闭
 	close(fd);dd
+	return 0;
 }
--- UNIX/样例代码/socket编程模型/本地socket/socketB.c
+++ UNIX/样例代码/socket编程模型/本地socket/socketB.c
@@ -4,30 +4,26 @@
 #include <linux/un.h>
 #include <string.h>
 #include <unistd.h>
-main()
-{
+void main() {
 	int fd;
 	int r;
 	char buf[100];
-	struct sockaddr_un addr={0};
+	struct sockaddr_un addr = {0};
 	//1.建立socket
 	//fd=socket(AF_UNIX,SOCK_DGRAM,0);
-	fd=socket(AF_UNIX,SOCK_STREAM,0);
+	fd = socket(AF_UNIX,SOCK_STREAM, 0);
 	//2.连接到指定的地址
-	addr.sun_family=AF_UNIX;
-	memcpy(addr.sun_path,"cs.sock",
-			strlen("cs.sock"));
-	r=connect(fd,(struct sockaddr*)&addr,
-			sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	memcpy(addr.sun_path, "cs.sock", strlen("cs.sock"));
+	r = connect(fd, (struct sockaddr*)&addr, sizeof(addr));
 	//3.发送数据
-	while(1)
-	{
-		write(fd,"Hello!MaomaoYu!",
-			strlen("Hello!MaomaoYu!"));
-		read(fd,buf,100);
-		printf("%s\n",buf);
+	while (1) {
+		write(fd, "Hello!MaomaoYu!", strlen("Hello!MaomaoYu!"));
+		read(fd, buf, 100);
+		printf("%s\n", buf);
 		sleep(1);	
 	}
 	//4.关闭
 	close(fd);
+	return 0;
 }
