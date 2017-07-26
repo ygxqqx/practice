33422445b83f72c7e060397be1802491e0e5448d
luoxupan <ygxqqx@163.com>
2017-07-26 18:10:44 +0800

    初级的可以运行的版本

--- tiny-reactjs/tiny-react.js
+++ tiny-reactjs/tiny-react.js
@@ -73,6 +73,54 @@ ReactDOMComponent.prototype.mountComponent = function(rootID) {
     return tagOpen + '>' + content + tagClose;
 }
 
+function ReactCompositeComponent(element) {
+    // 存放元素element对象
+    this._currentElement = element;
+    // 存放唯一标识
+    this._rootNodeID = null;
+    // 存放对应的ReactClass的实例
+    this._instance = null;
+}
+// 用于返回当前自定义元素渲染时应该返回的内容
+ReactCompositeComponent.prototype.mountComponent = function(rootID) {
+    this._rootNodeID = rootID;
+    // 拿到当前元素对应的属性值
+    let publicProps = this._currentElement.props;
+    console.log(publicProps);
+    // 拿到对应的ReactClass
+    let ReactClass = this._currentElement.type;
+    console.log(this._currentElement.type);
+    // Initialize the public class
+    let inst = new ReactClass(publicProps);
+    this._instance = inst;
+    // 保留对当前comonent的引用，下面更新会用到
+    inst._reactInternalInstance = this;
+    if (inst.componentWillMount) {
+        inst.componentWillMount();
+        // 这里在原始的reactjs其实还有一层处理，就是  componentWillMount调用setstate，不会触发rerender而是自动提前合并，这里为了保持简单，就略去了
+    }
+    // 调用ReactClass的实例的render方法,返回一个element或者一个文本节点
+    let renderedElement = this._instance.render();
+    // 得到renderedElement对应的component类实例
+    let renderedComponentInstance = instantiateReactComponent(renderedElement);
+    this._renderedComponent = renderedComponentInstance; // 存起来留作后用
+    // 拿到渲染之后的字符串内容，将当前的_rootNodeID传给render出的节点
+    let renderedMarkup = renderedComponentInstance.mountComponent(this._rootNodeID);
+    // 之前我们在React.render方法最后触发了mountReady事件，所以这里可以监听，在渲染完成后会触发。
+    $(document).on('mountReady', function() {
+        // 调用inst.componentDidMount
+        inst.componentDidMount && inst.componentDidMount();
+    });
+    return renderedMarkup;
+}
+
+// 定义ReactClass类,所有自定义的超级父类
+let ReactClass = function() {
+
+}
+
+// 留给子类去继承覆盖
+ReactClass.prototype.render = function() {}
 
 // component工厂  用来返回一个component实例
 function instantiateReactComponent(node) {
@@ -84,6 +132,11 @@ function instantiateReactComponent(node) {
     if (typeof node == 'object' && typeof node.type == 'string') {
         // 注意这里，使用了一种新的component
         return new ReactDOMComponent(node);
+    }
+    // 自定义的元素节点
+    if (typeof node === 'object' && typeof node.type === 'function') {
+        // 注意这里，使用新的component,专门针对自定义元素
+        return new ReactCompositeComponent(node);
     }
 }
 
@@ -122,6 +175,19 @@ React = {
             props.children = childArray;
         }
         return new ReactElement(type, key, props);
+    },
+    createClass: function(spec) {
+        // 生成一个子类
+        let Constructor = function(props) {
+            this.props = props;
+            this.state = this.getInitialState ? this.getInitialState() : null;
+        }
+        // 原型继承，继承超级父类
+        Constructor.prototype = new ReactClass();
+        Constructor.prototype.constructor = Constructor;
+        // 混入spec到原型
+        $.extend(Constructor.prototype, spec);
+        return Constructor;
     },
     render: function(element, container) {
         let componentInstance = instantiateReactComponent(element);
