11a82ada671bc6f27f383bb2582866608a72a674
luoxupan <ygxqqx@163.com>
2017-07-26 11:16:54 +0800

    增加虚拟dom reactElement定义

--- tiny-reactjs/tiny-react.js
+++ tiny-reactjs/tiny-react.js
@@ -1,3 +1,5 @@
+// 后续可以分模块编写(每个功能一个模块，每个模块分为一个文件) 用 webpack 打包生成一个文件即可
+
 // component类，用来表示文本在渲染，更新，删除时应该做些什么事情
 function ReactDOMTextComponent(text) {
     // 存下当前的字符串
@@ -12,13 +14,76 @@ ReactDOMTextComponent.prototype.mountComponent = function(rootID) {
     return '<span data-reactid="' + rootID + '">' + this._currentElement + '</span>';
 };
 
+// component类，用来表示文本在渲染，更新，删除时应该做些什么事情
+function ReactDOMComponent(element) {
+    // 存下当前的element对象引用
+    this._currentElement = element;
+    this._rootNodeID = null;
+}
+
+// component渲染时生成的dom结构
+ReactDOMComponent.prototype.mountComponent(rootID) {
+    // 赋值标识
+    this._rootNodeID = rootID;
+    let props = this._currentElement.props;
+    let tagOpen = '<' + this._currentElement.type;
+    let tagClose = '</' + this._currentElement.type + '>';
+    // 加上reactid标识
+    tagOpen += ' data-reactid=' + this._rootNodeID;
+    // 拼凑出属性
+    for (let propKey in props) {
+        // 这里要做一下事件的监听，就是从属性props里面解析拿出on开头的事件属性的对应事件监听
+        if (/^on[A-Za-z]/.test(propKey)) {
+            let eventType = propKey.replace('on', '');
+            // 针对当前的节点添加事件代理,以_rootNodeID为命名空间
+            $(document).delegate('[data-reactid="' + this._rootNodeID + '"]', eventType + '.' + this._rootNodeID, props[propKey]);
+        }
+        // 对于children属性以及事件监听的属性不需要进行字符串拼接
+        // 事件会代理到全局。这边不能拼到dom上不然会产生原生的事件监听
+        if (props[propKey] && propKey != 'children' && !/^on[A-Za-z]/.test(propKey)) {
+            tagOpen += ' ' + propKey + '=' + props[propKey];
+        }
+    }
+    // 获取子节点渲染出的内容
+    let content = '';
+    let children = props.children || [];
+    let childrenInstances = []; // 用于保存所有的子节点的componet实例，以后会用到
+    let that = this;
+    for (let key in children) {
+        if (children.hasOwnProperty(key)) {
+            let child = children[key];
+            // 这里再次调用了instantiateReactComponent实例化子节点component类，拼接好返回
+            let childComponentInstance = instantiateReactComponent(child);
+            childComponentInstance._mountIndex = key;
+
+            childrenInstances.push(childComponentInstance);
+            // 子节点的rootId是父节点的rootId加上新的key也就是顺序的值拼成的新值
+            let curRootId = that._rootNodeID + '.' + key;
+            // 得到子节点的渲染内容
+            let childMarkup = childComponentInstance.mountComponent(curRootId);
+            // 拼接在一起
+            content += ' ' + childMarkup;
+        }
+    }
+
+    // 留给以后更新时用的这边先不用管
+    this._renderedChildren = childrenInstances;
+
+    // 拼出整个html内容
+    return tagOpen + '>' + content + tagClose;
+}
+
+
 // component工厂  用来返回一个component实例
 function instantiateReactComponent(node) {
+    // 文本节点的情况
     if (typeof node == 'string' || typeof node == 'number') {
         return new ReactDOMTextComponent(node);
     }
-    if (typeof node == 'object' || typeof node.type == 'string') {
-
+    // 浏览器默认节点的情况
+    if (typeof node == 'object' && typeof node.type == 'string') {
+        // 注意这里，使用了一种新的component
+        return new ReactDOMComponent(node);
     }
 }
 
@@ -56,6 +121,7 @@ React = {
             }
             props.children = childArray;
         }
+        return new ReactElement(type, key, props);
     },
     render: function(element, container) {
         let componentInstance = instantiateReactComponent(element);
